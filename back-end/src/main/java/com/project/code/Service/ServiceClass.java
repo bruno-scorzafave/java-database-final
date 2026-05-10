d3bac4c7cf3dc644505431b57373e5e21ca21977
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2026-05-10 19:16:09 -0400

    Create ServiceClass

--- back-end/src/main/java/com/project/code/Service/ServiceClass.java
+++ back-end/src/main/java/com/project/code/Service/ServiceClass.java
@@ -1,8 +1,58 @@
 package com.project.code.Service;
 
+import com.project.code.Repo.ProductRepository;
+import com.project.code.Repo.InventoryRepository;
+import com.project.code.Model.Product;
+import com.project.code.Model.Inventory;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceClass {
     
+    private final InventoryRepository inventoryRepository;
+    private final ProductRepository productRepository;
+
+    public ServiceClass(InventoryRepository inventoryRepository, ProductRepository productRepository) {
+        this.inventoryRepository = inventoryRepository;
+        this.productRepository = productRepository;
+    }
+
+    public boolean validateInventory(Inventory inventory) {
+        Inventory result = inventoryRepository.findByProductIdandStoreId(inventory.getProduct().getId(), inventory.getStore().getId());
+
+        if(result == null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean validateProduct(Product product) {
+        Product result = productRepository.findByName(product.getName());
+
+        if(result == null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean validateProductId(Long id) {
+        Product result = productRepository.findByid(product.getId());
+
+        if(result == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean getInventoryId(Inventory inventory) {
+        Inventory result = inventoryRepository.findByProductIdandStoreId(inventory.getProduct().getId(), inventory.getStore().getId());
+
+        return result;
+    }
 // 1. **validateInventory Method**:
 //    - Checks if an inventory record exists for a given product and store combination.
 //    - Parameters: `Inventory inventory`
