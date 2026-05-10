e53c212c78462d5df0267399b4a56b731e56cdda
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2026-05-10 15:35:01 -0400

    Add @Repository annotation

--- back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -3,7 +3,8 @@ package com.project.code.Repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> >
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     public Customer findByEmail(String email);
 
