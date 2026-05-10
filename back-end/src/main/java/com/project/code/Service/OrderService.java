070681f814adde534e5c5b2fcc9a80763b3e3bb7
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2026-05-10 18:20:40 -0400

    Start creating Oder service

--- back-end/src/main/java/com/project/code/Service/OrderService.java
+++ back-end/src/main/java/com/project/code/Service/OrderService.java
@@ -1,7 +1,67 @@
 package com.project.code.Service;
 
+import com.project.code.Repo.ProductRepository;
+import com.project.code.Repo.InventoryRepository;
+import com.project.code.Repo.CustomerRepository;
+import com.project.code.Repo.StoreRepository;
+import com.project.code.Repo.OrderDetailsRepository;
+import com.project.code.Repo.OrderItemRepository;
+import com.project.code.Model.PlaceOrderRequestDTO;
+import com.project.code.Model.PurchaseProductDTO;
+import com.project.code.Model.Customer;
+import com.project.code.Model.Store;
+import com.project.code.Model.OrderDetails;
+import com.project.code.Model.OrderItem;
+import com.project.code.Model.Product;
 
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @Autowired
+    private CustumerRepository customerRepository;
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Autowired
+    private OrderDetailsRepository oderDetailsRepository;
+
+    @Autowired
+    private OrderItemRepository OrderItemRepository;
+
+    public void saveOrder(PlaceOrderRequestDTO placeOrderRequestDTO) {
+        Customer customer = customerRepository.findByEmail(placeOrderRequestDTO.getCustomerEmail());
+
+        if(customer == null) {
+            customer = new Customer();
+            customer.setName(placeOrderRequestDTO.getCostumerName());
+            customer.setEmail(placeOrderRequestDTO.getCostumerEmail());
+            customer.setPhone(placeOrderRequestDTO.getCostumerPhone());
+        }
+
+        customerRepository.save(customer);
+
+        Store store = storeRepository.findById(placeOrderRequest.getStoreId()).orElseThrow(() -> new RuntimeException("Store not found"));
+
+        OrderDetails oderDetails = new OrderDetails(customer, store, placeOrderRequestDTO.getTotalPrice(), java.time.LocalDateTime.now());
+        orderDetailsRepository.save(oderDetails);
+
+        for(PurchaseProductDTO purchaseProduct : placeOrderRequestDTO.getPurchaseProduct()) {
+            Product product = productRepository.findById(purchaseProduct.getId());
+            // OrderItem orderItem = new OrderItem(oderDetails, product, purchaseProduct);
+        }
+    }
+
 // 1. **saveOrder Method**:
 //    - Processes a customer's order, including saving the order details and associated items.
 //    - Parameters: `PlaceOrderRequestDTO placeOrderRequest` (Request data for placing an order)
