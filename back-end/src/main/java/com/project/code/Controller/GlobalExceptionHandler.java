11e378cd0e3de21e9b96f0ba625d58ca05da6370
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2026-05-12 15:50:05 -0400

    Remove comments

--- back-end/src/main/java/com/project/code/Controller/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/project/code/Controller/GlobalExceptionHandler.java
@@ -21,11 +21,4 @@ public class GlobalExceptionHandler {
         
         return map;
     }
-// 2. Define the `handleJsonParseException` Method:
-//    - Annotate with `@ExceptionHandler(HttpMessageNotReadableException.class)` to handle cases where the request body is not correctly formatted (e.g., invalid JSON).
-//    - The `HttpMessageNotReadableException` typically occurs when the input data cannot be deserialized or is improperly formatted.
-//    - Use `@ResponseStatus(HttpStatus.BAD_REQUEST)` to specify that the response status will be **400 Bad Request** when this exception is thrown.
-//    - The method should return a `Map<String, Object>` with the following key:
-//        - **`message`**: The error message should indicate that the input provided is invalid. The value should be `"Invalid input: The data provided is not valid."`.
-    
 }
