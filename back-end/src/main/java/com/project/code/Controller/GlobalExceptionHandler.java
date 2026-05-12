94fe134e459c89460a31a1f6fcb0fb1758c866d9
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2026-05-12 15:49:27 -0400

    Create GlobalExceptionHandler

--- back-end/src/main/java/com/project/code/Controller/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/project/code/Controller/GlobalExceptionHandler.java
@@ -1,13 +1,26 @@
 package com.project.code.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-// 1. Set Up the Global Exception Handler Class:
-//    - Annotate the class with `@RestControllerAdvice` to enable global exception handling for REST controllers.
-//    - This allows the class to handle exceptions thrown in any of the controllers globally.
-
-
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleJsonParseException() {
+        Map<String, Object> map = new HashMap<>();
+        
+        map.put("message", "Invalid input: The data provided is not valid.");
+        
+        return map;
+    }
 // 2. Define the `handleJsonParseException` Method:
 //    - Annotate with `@ExceptionHandler(HttpMessageNotReadableException.class)` to handle cases where the request body is not correctly formatted (e.g., invalid JSON).
 //    - The `HttpMessageNotReadableException` typically occurs when the input data cannot be deserialized or is improperly formatted.
